a40da314f748cf90ab92cfe511850ce754874077
zuchermann <yn@lawn-128-61-28-191.lawn.gatech.edu>
2016-09-08 14:48:54 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.all('/*', function(req, res){
+	res.send('\
+		<!DOCTYPE html>\
+		<html>\
+			<head>\
+				<title>Mashup App</title>\
+				<base href="/">\
+				<body>\
+					<div ui-view></div>\
+					<script src="bundle.js"></script>\
+				</body>\
+			</head>\
+		</html>\
+		');
+});
+
+app.listen(PORT, function() {
+	console.log("Server running on port " + PORT);
+});
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import angular from 'angular';
+import appModule from 'config';
+import 'css/master.scss';
+
+
+angular.bootstrap(document, [appModule.name]);
